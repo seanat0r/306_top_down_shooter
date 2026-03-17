f5862d3888bbe95f2bf941f27af50066b33e1742
seanat0r <seanatorhd@gmail.com>
2026-03-17 18:59:34 +0100

    debuging: adding test modules, changed layout: main.py and config.py is now in src/; path are updated in all modul

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,1 @@
 # from .file import Function/ Classe
-from .player import Player
-from .projectile import Projectile
-from .enemy import Enemy
-from .level_manager import LevelOne, LevelTwo
-from .ui_manager import UIManager
